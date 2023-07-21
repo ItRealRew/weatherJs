c25f1a401bad4c343018d32ac8e2ae9cd746d00f
ItRealRew <rew-proninmax@yandex.ru>
2023-07-21 19:14:03 +0300

    Начал метод добавления в дом дерево

---
+++ index.js
@@ -0,0 +1,51 @@
+function AddNew() {
+    const parentDiv = document.getElementsByClassName("grid")[0];
+
+    const newEl = document.createElement("div");
+
+    const DateDiv = document.createElement("div");
+    DateDiv.textContent = new Date();
+
+    const PlaceDiv = document.createElement("div");
+    PlaceDiv.textContent = "Владимир";
+
+    const TemperatureDiv = document.createElement("div");
+    TemperatureDiv.textContent = "24 градусов";
+
+    const CloudСoverDiv = document.createElement("div");
+    CloudСoverDiv.textContent = "Да";
+
+    const SunshineDiv = document.createElement("div");
+    SunshineDiv.textContent = "Да";
+
+    const HumidityDiv = document.createElement("div");
+    HumidityDiv.textContent = "Да";
+
+    const WindDiv = document.createElement("div");
+    WindDiv.textContent = "Северный";
+
+    const CycloneDiv = document.createElement("div");
+    CycloneDiv.textContent = "Нет";
+
+    const AntiCycloneDiv = document.createElement("div");
+    AntiCycloneDiv.textContent = "Нет";
+
+    const AuthorDiv = document.createElement("div");
+    AuthorDiv.textContent = "ItRelRew";
+
+    newEl.appendChild(DateDiv);
+    newEl.appendChild(PlaceDiv);
+    newEl.appendChild(TemperatureDiv);
+    newEl.appendChild(CloudСoverDiv);
+    newEl.appendChild(SunshineDiv);
+    newEl.appendChild(HumidityDiv);
+    newEl.appendChild(WindDiv);
+    newEl.appendChild(CycloneDiv);
+    newEl.appendChild(AntiCycloneDiv);
+    newEl.appendChild(AuthorDiv);
+
+    newEl.classList.add("grid-body");
+    newEl.classList.add("transition")
+
+    parentDiv.appendChild(newEl);
+}
